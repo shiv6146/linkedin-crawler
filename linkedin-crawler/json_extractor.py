992ea6334f8c09b76b94c1f9b6a4a48340f644ed
Shiva shankar <siv6146@gmail.com>
2014-07-29 05:42:14 +0530

    Initial commit

---
+++ linkedin-crawler/json_extractor.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+
+def returnDetails(cmp_name,n):
+    '''(str)->(dict(int:[list]))
+    This function takes in company name to be searched as argument, and returns
+    the id of the employees as keys mapped to their basic info
+    '''
+    path="Enter your full path where the script is located" #Even on Windows use only forward slashes (Eg):"C:/Python27/Scripts/scraping/profoundis/"
+    dom_filename="dom.txt"
+    pattern_start='<code id="voltron_srp_main-content" style="display:none;"><!--' #This is the pattern that uniquely distinguishes HTML and JSON in the page source
+    pattern_end='--></code>' #This is the delimiter which denotes the end of JSON
+    details=[] #Details of employees
+    d={} #Linkedin employee-id mapped to the details of the respective employees
+
+    if(os.path.isfile(path+dom_filename)):
+        f=open(dom_filename,"r")
+        dom=f.read()
+        json_content=dom[dom.find(pattern_start)+len(pattern_start):dom.find(pattern_end)]
+        f.close()
+        json_data=json.loads(json_content)
+        for i in range(n):
+            if(cmp_name.capitalize() in json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["fmt_headline"]):
+                i_d=json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["id"]
+                name=str(json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["fmt_name"])
+                ind=str(json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["fmt_industry"])
+                loc=str(json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["fmt_location"])
+                details.append(name)
+                details.append(ind)
+                details.append(loc)
+                snippets=json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["snippets"]
+                if(len(snippets)>0):
+                    for j in range(len(snippets)):
+                        temp=snippets[j]["bodyList"]
+                        temp=[str(s).replace('<strong class="highlight">','').replace('</strong>','').replace('&amp;','&') for s in temp]
+                        details.append(str(snippets[j]["fieldName"])+' -> '+temp)
+                    d[i_d]=details
+                    details=[]
+                else:
+                    desig=str(json_data["content"]["page"]["voltron_unified_search_json"]["search"]["results"][i]["person"]["fmt_headline"]).replace('<strong class="highlight">','').replace('</strong>','').replace('&amp;','&')
+                    details.append(desig)
+                    d[i_d]=details
+                    details=[]
+    return d
---
+++ linkedin-crawler/linkedin.py
@@ -0,0 +1,104 @@
+import cookielib
+import os
+import urllib2
+import urllib
+import re
+import json_extractor #Custom written module, that extracts JSON from the DOM
+from bs4 import BeautifulSoup
+
+username = "username@email.com" #Enter the linkedin username
+password = "password" #Enter the linkedin password
+
+cookie_filename = "parser.cookies.txt" #Cookies set by linkedin are stored
+dom_filename="dom.txt" #The entire DOM of the page currently viewed is stored
+path="Enter your full path where the script is located" #Even on Windows use only forward slashes (Eg):"C:/Python27/Scripts/scraping/profoundis/"
+
+class LinkedInParser(object):
+
+    def __init__(self, login, password):
+        if(os.path.isfile(path+cookie_filename)):
+            os.remove(path+cookie_filename)
+        """ Start up... """
+        self.login = login
+        self.password = password
+
+        # Simulate browser with cookies enabled
+        self.cj = cookielib.MozillaCookieJar(cookie_filename)
+        if os.access(cookie_filename, os.F_OK):
+            self.cj.load()
+        self.opener = urllib2.build_opener(
+            urllib2.HTTPRedirectHandler(),
+            urllib2.HTTPHandler(debuglevel=0),
+            urllib2.HTTPSHandler(debuglevel=0),
+            urllib2.HTTPCookieProcessor(self.cj)
+        )
+        self.opener.addheaders = [
+            ('User-agent', ('Mozilla/4.0 (compatible; MSIE 6.0; '
+                           'Windows NT 5.2; .NET CLR 1.1.4322)'))
+        ]
+
+        # Login
+        self.loginPage()
+
+        self.cj.save()
+
+
+    def loadPage(self, url, data=None):
+        """
+        Utility function to load HTML from URLs for us with hack to continue despite 404
+        """
+        # We'll print the url in case of infinite loop
+        # print "Loading URL: %s" % url
+        try:
+            if data is not None:
+                response = self.opener.open(url, data)
+            else:
+                response = self.opener.open(url)
+            return ''.join(response.readlines())
+        except:
+            # If URL doesn't load for ANY reason, try again...
+            # Quick and dirty solution for 404 returns because of network problems
+            # However, this could infinite loop if there's an actual problem
+            return self.loadPage(url, data)
+
+    def loginPage(self):
+        """
+        Handle login. This should populate our cookie jar.
+        """
+        html = self.loadPage("https://www.linkedin.com/")
+        soup = BeautifulSoup(html)
+        csrf = soup.find(id="loginCsrfParam-login")['value']
+
+        login_data = urllib.urlencode({
+            'session_key': self.login,
+            'session_password': self.password,
+            'loginCsrfParam': csrf,
+        })
+
+        html = self.loadPage("https://www.linkedin.com/uas/login-submit", login_data)
+        return
+
+
+    def searchCompany(self,name):
+        html=self.loadPage("http://www.linkedin.com/vsearch/p?type=people&keywords="+name)
+        soup=BeautifulSoup(html)
+        if(os.path.isfile(path+dom_filename)):
+            os.remove(path+dom_filename)
+        f=open("dom.txt","a")
+        f.write(str(soup))
+        f.close()
+
+parser = LinkedInParser(username, password) #Creating an instance for LinkedInParser, and the constructor takes in username,password set above as parameters to login
+
+print "Enter company name to search: "
+name = raw_input()
+print "Enter the required number of employees: "
+n=int(raw_input())
+parser.searchCompany(name) #This loads the search page with list of matching company and writes the entire page source to file "dom.txt"
+d=json_extractor.returnDetails(name,n) #Since, the page has dynamic links its impossible to parse the DOM to get through, so this extracts the JSON from "dom.txt" and then parses it
+
+for i in d:
+    print d[i][0]
+    for j in range(1,len(d[i])):
+        print d[i][j]
+    print '*'*100
